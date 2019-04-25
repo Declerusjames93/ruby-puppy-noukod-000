e4b4bf71c24447842ca8198f5ff9c3a9821977ef
James Declerus <james.declerus@esih.edu>
2019-04-25 13:41:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,18 +6,18 @@ class Dog
     @@all << self
 
   end
-  
-  @@dogs_name = Array.new 
+
+  @@dogs_name = Array.new
 def all
-  all.each {|x|    
+  all.each {|x|
 dogs_name << x
   }
-  
+
 end
 
  def clear_all
-   
+
    dogs_name.clear
  end
 
-end 
+end
